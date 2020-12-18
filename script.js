83381eef99cd3c2922f8d7d4e2f4e2138c4af8f3
Luis <m.luishernandez@outlook.com>
2020-12-18 18:39:53 -0500

    made if else statement to select criteria

--- script.js
+++ script.js
@@ -1,94 +1,24 @@
 // Array of special characters to be included in password
-var specialCharacters = ['@', '%', '+', '\\', '/',
-  "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
-];
+var special = ['@', '%', '+', '\\', '/', "'", '!','#','$','^','?',':',',',')','(','}', '{',']','[','~','-','_','.'];
+
 // Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var numeric = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 // Array of lowercase characters to be included in password
-var lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
-];
+var lower = ['a','b','c','d','e', 'f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 // Array of uppercase characters to be included in password
-var upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
-];
+var upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X','Y','Z'];
 
 
 var answer = [];
 
-var hasSpecialCharacters;
-var hasLowerCaseCharacters;
-var hasUpperCaseCharacters;
-var hasNumericCharacters;
+var specialChosen;
+var lowerChosen;
+var upperChosen;
+var numericChosen;
 
-var confirmation;
+var arrayPlaceholder;
+
+//var confirmation;
 
 
 // Create variable to select the id generate on our html (button)
@@ -115,38 +45,114 @@ function generatePassword() {
 
 function getPasswordOptions() {
 
-  
-  confirm("Create a password");
-
   var quantity = parseInt(prompt("Choose from 8 to 128 characters"));
+// If statement that will check if the user input something or if it was the correct parameter of what was specified
+  if (!quantity){
+    alert("Need to input values");
+  }
+// else if statement defining 
+  else if (quantity < 8 || quantity > 128){
+    confirm("Input based on the parameters given");
 
-  if (quantity >= 8 && quantity <= 128) {
-    
-       hasSpecialCharacters = confirm("Click ok to confirm special characters");
-       hasNumericCharacters = confirm("Click ok to confirm numeric characters");
-       hasLowerCaseCharacters = confirm("Click ok to confirm lower case characters");
-       hasUpperCaseCharacters = confirm("Click ok to confirm upper case characters");
+  }
 
-    for (var i = 0; i < quantity; i++){
-     
-      var random = specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+  else{
+
+    specialChosen = confirm("Will this contain special characters?");
+    upperChosen = confirm("Will this contain uppercase letters?");
+    lowerChosen = confirm("Will this contain lowercase letters?");
+    numericChosen = confirm("Will this contain numbers?");
+
+  }
+
+  if (!specialChosen, !upperChosen, !lowerChosen, !numericChosen){
+    alert("Must choose a at least one criteria");
+  }
+//accounts for everything
+  else if (specialChosen && upperChosen && lowerChosen && numericChosen){
+    arrayPlaceholder = special.concat(upper, lower, numeric);
+  }
+// special,upper and lower
+  else if (specialChosen && upperChosen && lowerChosen){
+    arrayPlaceholder = special.concat(upper, lower);
+  }
+//special, numeric, lower
+  else if (specialChosen && numericChosen && lowerChosen){
+    arrayPlaceholder = special.concat(numeric, lower);
+  }
+ //special upper, numeric
+  else if (specialChosen && upperChosen && numericChosen){
+    arrayPlaceholder = special.concat(upper, numeric);
+  }
+// special, numeric
+  else if (specialChosen && numericChosen){
+    arrayPlaceholder = special.concat(numeric);
+  }
+// special upper
+  else if (specialChosen && upperChosen){
+    arrayPlaceholder = special.concat(upper);
+  }
+// special lower
+  else if (specialChosen && lowerChosen){
+    arrayPlaceholder = special.concat(lower);
+  }
+//special
+  else if (specialChosen){
+    arrayPlaceholder = special;
+  }
+
+  //Start Upper if elses
+
+// upper, lower, numeric
+  else if (lowerChosen && numericChosen){
+    arrayPlaceholder = upper.concat(lower,numeric);
+  }
+// upper lower
+  else if (upperChosen && lowerChosen){
+    arrayPlaceholder = upper.concat(lower);
+  }
+//upper numeric
+  else if (numericChosen){
+    arrayPlaceholder = upper.concat(numeric);
+  }
+//upper
+  else if (upperChosen){
+    arrayPlaceholder = upper;
+  }
+// start of lowers
+
+//lower numeric
+  else if (lowerChosen && numericChosen){
+    arrayPlaceholder = upper.concat(lower,numeric);
+  }
+// lower
+  else if (lowerChosen){
+    arrayPlaceholder = lower;
+  }
+//numeric
+  else if (numericChosen){
+    arrayPlaceholder = numeric;
+  }
+
+
+
+  for (var i = 0; i < quantity; i++){
+
+    var random = arrayPlaceholder[Math.floor(Math.random() * arrayPlaceholder.length)];
       answer.push(random);
-      console.log(random); 
-    }
+      console.log(random);
 
-   
+  }
     var password = answer.join("");
-  
+    
     return password;
 
+
   }
 
-  else {
+ 
 
-    confirm("Invalid amount");
-  }
 
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
